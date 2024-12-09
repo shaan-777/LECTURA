ed5abcbfe012ad2dea96a6dbdd0011b262b19231
Vasu <148642918+Vasu-Varshneya@users.noreply.github.com>
2024-12-09 20:06:25 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,17 @@
-'use client';
-import Navbar from "@/components/landingpage/Navbar";
+
+import Hero from '../components/Hero'
+import Features from '../components/Features'
+import Navbar from '../components/landingpage/Navbar'
+import Pricing from '../components/pricing'
+import Footer from '../components/footer'
 export default function Home() {
-  return(
-    <div>
-      <Navbar />
-    </div>
+  return (
+    <main className="container min-h-screen ">
+      <Navbar/>
+      <Hero />
+      <Features />
+      <Pricing/>
+      <Footer/>
+    </main>
   )
 }
