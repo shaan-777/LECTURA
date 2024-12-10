044469f94fcadad0a3c0edf0e09fbf8110987f88
Vasu-Varshneya <148642918+Vasu-Varshneya@users.noreply.github.com>
2024-12-10 18:16:36 +0530

    next

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 // next.config.js (ESM syntax)
 export default {
+     
     images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '**', // Allow images from any domain
+        },
+      ],
       domains: ['cdn.prod.website-files.com'],
     },
   }
