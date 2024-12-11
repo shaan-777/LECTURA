7efab6b39bb8679229d796f0f04eb1924810b2b2
Mohit2005123 <mohitmongia2005@gmail.com>
2024-12-11 23:16:25 +0530

    Added navbar in login and signup

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -6,6 +6,7 @@ import { GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, signOu
 import { Checkbox } from "@/components/ui/checkbox"
 import { db } from '../../lib/firebase';
 import { collection, query, where, getDocs } from 'firebase/firestore';
+import Navbar from '../../components/landingpage/Navbar';
 
 export default function Login() {
   const [email, setEmail] = useState('');
@@ -75,87 +76,90 @@ export default function Login() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#111111] py-8">
-      <div className="max-w-6xl w-full bg-[#1A1A1A] shadow-xl rounded-lg flex my-2">
-        <div className="w-3/5 p-16">
-          <h1 className="text-4xl font-poppins mb-8 text-[50px] font-semibold text-white">
-            Welcome Back
-          </h1>
-          <p className="text-[#9CA3AF] mb-8 font-poppins font-light text-[20px]">
-            Log in to access your personalized study materials and tools.
-          </p>
-          <button
-            onClick={handleGoogleLogin}
-            className="w-full py-5 px-4 mb-8 bg-[#2A2A2A] border border-[#333333] text-white rounded-full flex items-center justify-center shadow-sm hover:shadow-xl transition duration-300"
-          >
-            <img
-              src="/signup/google-icon.svg"
-              alt="Google Icon"
-              className="h-6 mr-2"
-            />
-            Continue with Google
-          </button>
-          <div className="flex items-center justify-between mb-4">
-            <hr className="w-1/3 border-[#333333]" />
-            <span className="text-[#9CA3AF]">OR</span>
-            <hr className="w-1/3 border-[#333333]" />
-          </div>
-          <form className="space-y-8" onSubmit={handleEmailLogin}>
-            <input
-              type="email"
-              placeholder="Email address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full py-4 px-4 border border-[#333333] bg-[#2A2A2A] text-white rounded-lg focus:outline-none focus:ring focus:ring-[#4B5563] placeholder-[#9CA3AF]"
-            />
-            <div className="space-y-2">
+    <div className="min-h-screen bg-[#111111]">
+      <Navbar />
+      <div className="flex items-center justify-center py-8 pt-24">
+        <div className="max-w-6xl w-full bg-[#1A1A1A] shadow-xl rounded-lg flex my-2">
+          <div className="w-3/5 p-16">
+            <h1 className="text-4xl font-poppins mb-8 text-[50px] font-semibold text-white">
+              Welcome Back
+            </h1>
+            <p className="text-[#9CA3AF] mb-8 font-poppins font-light text-[20px]">
+              Log in to access your personalized study materials and tools.
+            </p>
+            <button
+              onClick={handleGoogleLogin}
+              className="w-full py-5 px-4 mb-8 bg-[#2A2A2A] border border-[#333333] text-white rounded-full flex items-center justify-center shadow-sm hover:shadow-xl transition duration-300"
+            >
+              <img
+                src="/signup/google-icon.svg"
+                alt="Google Icon"
+                className="h-6 mr-2"
+              />
+              Continue with Google
+            </button>
+            <div className="flex items-center justify-between mb-4">
+              <hr className="w-1/3 border-[#333333]" />
+              <span className="text-[#9CA3AF]">OR</span>
+              <hr className="w-1/3 border-[#333333]" />
+            </div>
+            <form className="space-y-8" onSubmit={handleEmailLogin}>
               <input
-                type={showPassword ? "text" : "password"}
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                type="email"
+                placeholder="Email address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className="w-full py-4 px-4 border border-[#333333] bg-[#2A2A2A] text-white rounded-lg focus:outline-none focus:ring focus:ring-[#4B5563] placeholder-[#9CA3AF]"
               />
-              <div className="flex items-center space-x-2">
-                <Checkbox
-                  id="showPassword"
-                  checked={showPassword}
-                  onCheckedChange={setShowPassword}
-                  className="border-[#333333] data-[state=checked]:bg-white data-[state=checked]:text-black transition-all duration-200 hover:scale-110 data-[state=checked]:scale-105 hover:border-white"
+              <div className="space-y-2">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full py-4 px-4 border border-[#333333] bg-[#2A2A2A] text-white rounded-lg focus:outline-none focus:ring focus:ring-[#4B5563] placeholder-[#9CA3AF]"
                 />
-                <label
-                  htmlFor="showPassword"
-                  className="text-sm font-medium leading-none text-[#9CA3AF] peer-disabled:cursor-not-allowed peer-disabled:opacity-70 transition-colors duration-200 hover:text-white"
-                >
-                  Show password
-                </label>
+                <div className="flex items-center space-x-2">
+                  <Checkbox
+                    id="showPassword"
+                    checked={showPassword}
+                    onCheckedChange={setShowPassword}
+                    className="border-[#333333] data-[state=checked]:bg-white data-[state=checked]:text-black transition-all duration-200 hover:scale-110 data-[state=checked]:scale-105 hover:border-white"
+                  />
+                  <label
+                    htmlFor="showPassword"
+                    className="text-sm font-medium leading-none text-[#9CA3AF] peer-disabled:cursor-not-allowed peer-disabled:opacity-70 transition-colors duration-200 hover:text-white"
+                  >
+                    Show password
+                  </label>
+                </div>
               </div>
-            </div>
-            <button
-              type="submit"
-              className="w-full py-4 px-4 bg-white text-black font-bold rounded-lg hover:bg-gray-100 transition duration-300"
-            >
-              Log In
-            </button>
-          </form>
-          {error && (
-            <div className="mt-4 p-3 bg-red-500/10 border border-red-500/50 rounded-lg">
-              <p className="text-red-500 text-sm text-center">{error}</p>
-            </div>
-          )}
-          <p className="text-sm text-[#9CA3AF] mt-4">
-            Don’t have an account?{' '}
-            <a href="/signup" className="underline hover:text-white transition duration-300">
-              Sign Up
-            </a>
-          </p>
-        </div>
-        <div className="w-2/5 bg-[#1A1A1A] flex items-center justify-center p-14 overflow-hidden">
-          <img
-            src="/signup/illustration.svg"
-            alt="Illustration"
-            className="w-full scale-125 object-contain"
-          />
+              <button
+                type="submit"
+                className="w-full py-4 px-4 bg-white text-black font-bold rounded-lg hover:bg-gray-100 transition duration-300"
+              >
+                Log In
+              </button>
+            </form>
+            {error && (
+              <div className="mt-4 p-3 bg-red-500/10 border border-red-500/50 rounded-lg">
+                <p className="text-red-500 text-sm text-center">{error}</p>
+              </div>
+            )}
+            <p className="text-sm text-[#9CA3AF] mt-4">
+              Don’t have an account?{' '}
+              <a href="/signup" className="underline hover:text-white transition duration-300">
+                Sign Up
+              </a>
+            </p>
+          </div>
+          <div className="w-2/5 bg-[#1A1A1A] flex items-center justify-center p-14 overflow-hidden">
+            <img
+              src="/signup/illustration.svg"
+              alt="Illustration"
+              className="w-full scale-125 object-contain"
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -6,7 +6,7 @@ import { GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword } f
 import { doc, setDoc, collection, query, where, getDocs } from 'firebase/firestore';
 import Link from 'next/link';
 import { Checkbox } from "@/components/ui/checkbox"
-
+import Navbar from '@/components/landingpage/Navbar';
 export default function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -109,104 +109,107 @@ export default function Signup() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#111111] py-4">
-      <div className="max-w-6xl w-full bg-[#1A1A1A] shadow-xl rounded-lg flex my-1">
-        <div className="w-3/5 p-12">
-          <h1 className="text-4xl font-poppins mb-6 text-[50px] font-semibold text-white">
-            Transform Your Study Experience
-          </h1>
-          <p className="text-[#9CA3AF] mb-6 font-poppins font-light text-[20px]">
-            Upload videos or notes and let AI create personalized study materials tailored just for you.
-          </p>
-          <button
-            onClick={handleGoogleSignup}
-            className="w-full py-4 px-4 mb-6 bg-[#2A2A2A] border border-[#333333] text-white rounded-full flex items-center justify-center shadow-sm hover:shadow-xl transition duration-300"
-          >
-            <img
-              src="/signup/google-icon.svg"
-              alt="Google Icon"
-              className="h-6 mr-2"
-            />
-            Continue with Google
-          </button>
-          <div className="flex items-center justify-between mb-3">
-            <hr className="w-1/3 border-[#333333]" />
-            <span className="text-[#9CA3AF]">OR</span>
-            <hr className="w-1/3 border-[#333333]" />
-          </div>
-          {isLoading && (
-            <div className="flex justify-center mb-4">
-              <div className="loader"></div>
-            </div>
-          )}
-          <form className="space-y-6" onSubmit={handleEmailSignup}>
-            <input
-              type="text"
-              placeholder="Full Name"
-              value={fullName}
-              onChange={(e) => setFullName(e.target.value)}
-              className="w-full py-3 px-4 border border-[#333333] bg-[#2A2A2A] text-white rounded-lg focus:outline-none focus:ring focus:ring-[#4B5563] placeholder-[#9CA3AF]"
-            />
-            <input
-              type="email"
-              placeholder="Email address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full py-3 px-4 border border-[#333333] bg-[#2A2A2A] text-white rounded-lg focus:outline-none focus:ring focus:ring-[#4B5563] placeholder-[#9CA3AF]"
-            />
-            <div className="space-y-2">
-              <input
-                type={showPassword ? "text" : "password"}
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="w-full py-3 px-4 border border-[#333333] bg-[#2A2A2A] text-white rounded-lg focus:outline-none focus:ring focus:ring-[#4B5563] placeholder-[#9CA3AF]"
-              />
-              <div className="flex items-center space-x-2">
-                <Checkbox
-                  id="showPassword"
-                  checked={showPassword}
-                  onCheckedChange={setShowPassword}
-                  className="border-[#333333] data-[state=checked]:bg-white data-[state=checked]:text-black transition-all duration-200 hover:scale-110 data-[state=checked]:scale-105 hover:border-white"
-                />
-                <label
-                  htmlFor="showPassword"
-                  className="text-sm font-medium leading-none text-[#9CA3AF] peer-disabled:cursor-not-allowed peer-disabled:opacity-70 transition-colors duration-200 hover:text-white"
-                >
-                  Show password
-                </label>
-              </div>
-            </div>
+    <>
+      <Navbar />
+      <div className="min-h-screen flex items-center justify-center bg-[#111111] py-4 pt-20">
+        <div className="max-w-6xl w-full bg-[#1A1A1A] shadow-xl rounded-lg flex my-1">
+          <div className="w-3/5 p-12">
+            <h1 className="text-4xl font-poppins mb-6 text-[50px] font-semibold text-white">
+              Transform Your Study Experience
+            </h1>
+            <p className="text-[#9CA3AF] mb-6 font-poppins font-light text-[20px]">
+              Upload videos or notes and let AI create personalized study materials tailored just for you.
+            </p>
             <button
-              type="submit"
-              className="w-full py-3 px-4 bg-white text-black font-bold rounded-lg hover:bg-gray-100 transition duration-300"
-              disabled={isLoading}
+              onClick={handleGoogleSignup}
+              className="w-full py-4 px-4 mb-6 bg-[#2A2A2A] border border-[#333333] text-white rounded-full flex items-center justify-center shadow-sm hover:shadow-xl transition duration-300"
             >
-              {isLoading ? (
-                <div className="loader w-4 h-4 border-t-black border-solid animate-spin"></div>
-              ) : (
-                "Sign Up"
-              )}
+              <img
+                src="/signup/google-icon.svg"
+                alt="Google Icon"
+                className="h-6 mr-2"
+              />
+              Continue with Google
             </button>
-            
-            {error && (
-              <div className="mt-2 p-3 bg-red-500/10 border border-red-500/50 rounded-lg">
-                <p className="text-red-500 text-sm text-center">{error}</p>
+            <div className="flex items-center justify-between mb-3">
+              <hr className="w-1/3 border-[#333333]" />
+              <span className="text-[#9CA3AF]">OR</span>
+              <hr className="w-1/3 border-[#333333]" />
+            </div>
+            {isLoading && (
+              <div className="flex justify-center mb-4">
+                <div className="loader"></div>
               </div>
             )}
-          </form>
-          <p className="text-sm text-[#9CA3AF] mt-3">
-            Already have an account? <Link href="/login" className="underline hover:text-gray-300 transition duration-300">Login</Link>
-          </p>
-        </div>
-        <div className="w-2/5 bg-[#1A1A1A] flex items-center justify-center p-12 overflow-hidden">
-          <img
-            src="/signup/illustration.svg"
-            alt="Illustration"
-            className="w-full scale-125 object-contain"
-          />
+            <form className="space-y-6" onSubmit={handleEmailSignup}>
+              <input
+                type="text"
+                placeholder="Full Name"
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+                className="w-full py-3 px-4 border border-[#333333] bg-[#2A2A2A] text-white rounded-lg focus:outline-none focus:ring focus:ring-[#4B5563] placeholder-[#9CA3AF]"
+              />
+              <input
+                type="email"
+                placeholder="Email address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full py-3 px-4 border border-[#333333] bg-[#2A2A2A] text-white rounded-lg focus:outline-none focus:ring focus:ring-[#4B5563] placeholder-[#9CA3AF]"
+              />
+              <div className="space-y-2">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full py-3 px-4 border border-[#333333] bg-[#2A2A2A] text-white rounded-lg focus:outline-none focus:ring focus:ring-[#4B5563] placeholder-[#9CA3AF]"
+                />
+                <div className="flex items-center space-x-2">
+                  <Checkbox
+                    id="showPassword"
+                    checked={showPassword}
+                    onCheckedChange={setShowPassword}
+                    className="border-[#333333] data-[state=checked]:bg-white data-[state=checked]:text-black transition-all duration-200 hover:scale-110 data-[state=checked]:scale-105 hover:border-white"
+                  />
+                  <label
+                    htmlFor="showPassword"
+                    className="text-sm font-medium leading-none text-[#9CA3AF] peer-disabled:cursor-not-allowed peer-disabled:opacity-70 transition-colors duration-200 hover:text-white"
+                  >
+                    Show password
+                  </label>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="w-full py-3 px-4 bg-white text-black font-bold rounded-lg hover:bg-gray-100 transition duration-300"
+                disabled={isLoading}
+              >
+                {isLoading ? (
+                  <div className="loader w-4 h-4 border-t-black border-solid animate-spin"></div>
+                ) : (
+                  "Sign Up"
+                )}
+              </button>
+              
+              {error && (
+                <div className="mt-2 p-3 bg-red-500/10 border border-red-500/50 rounded-lg">
+                  <p className="text-red-500 text-sm text-center">{error}</p>
+                </div>
+              )}
+            </form>
+            <p className="text-sm text-[#9CA3AF] mt-3">
+              Already have an account? <Link href="/login" className="underline hover:text-gray-300 transition duration-300">Login</Link>
+            </p>
+          </div>
+          <div className="w-2/5 bg-[#1A1A1A] flex items-center justify-center p-12 overflow-hidden">
+            <img
+              src="/signup/illustration.svg"
+              alt="Illustration"
+              className="w-full scale-125 object-contain"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
