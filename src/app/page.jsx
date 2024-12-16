3f0b16010d62e930d9bc2346a03faeb8ee7172a2
sparsh7637 <gulatisparsh0212@gmail.com>
2024-12-17 00:36:52 +0530

    trying testimonial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,8 @@ import Navbar from '../components/landingpage/Navbar'
 import Pricing from '../components/pricing'
 import Footer from '../components/footer'
 import {TimelineDemo}  from '@/components/usage'
+import { InfiniteMovingCardsDemo } from '@/components/testimonial'
+
 export default function Home() {
   return (
     <div className="w-[100%] bg-[#0f0f11] ">
@@ -13,6 +15,7 @@ export default function Home() {
       <Features />
       {/* <Pricing/> */}
       <TimelineDemo />
+      {/* <InfiniteMovingCardsDemo /> */}
       {/* <Footer/> */}
     </div>
   )
