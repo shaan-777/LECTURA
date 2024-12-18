cbb19d16a58a94f320619f602e0084e34e5d694b
sparsh7637 <gulatisparsh0212@gmail.com>
2024-12-18 19:26:11 +0530

    smoothened the dashboard page and flashcards

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -144,28 +144,33 @@ const DashboardPage = () => {
       </div>
 
       <div className="container mx-auto px-8 py-8 pt-24 max-w-[1200px] relative">
-        <div className="h-[10rem] w-full flex flex-col items-center p-5 mt-10 overflow-hidden rounded-md">
-          <h1 className="md:text-7xl text-xl lg:text-6xl font-bold text-center text-white relative z-20">
+        <div className="h-[15rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+          <h1 className="md:text-5xl text-5xl lg:text-6xl font-bold text-center text-white relative z-20">
             Dashboard
           </h1>
+          <div className="w-[40rem] h-40 relative">
+            {/* Gradients */}
+            <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+            <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+            <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+            <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+
+            {/* Core component */}
+            <SparklesCore
+              background="transparent"
+              minSize={0.4}
+              maxSize={1}
+              particleDensity={1200}
+              className="w-full h-full"
+              particleColor="#FFFFFF"
+            />
+
+            {/* Radial Gradient to prevent sharp edges */}
+            <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+          </div>
         </div>
 
-        <div
-          className="fixed inset-0 w-full h-full"
-          style={{ top: "200px", zIndex: 10 }}
-        >
-          <SparklesCore
-            background="transparent"
-            minSize={0.4}
-            maxSize={1.3}
-            particleDensity={1200}
-            className="w-full h-full"
-            particleColor="#FFFFFF"
-          />
-          <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
-        </div>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 mt-10 relative z-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 mt-0 relative z-20">
           {isLoading ? (
             <div className="col-span-full flex justify-center items-center min-h-[400px]">
               <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-purple-500"></div>
