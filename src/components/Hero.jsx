08edde99061a8e0760e782e3f86bf72d01dc6032
sparsh7637 <gulatisparsh0212@gmail.com>
2024-12-27 14:53:42 +0530

    added gradient

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,10 +18,10 @@ export default function Hero() {
   return (
     <section className="min-h-screen relative flex flex-col items-center justify-center bg-black px-4 py-16 pt-40 md:pt-32">
       {/* Background */}
-      {/* <div className="absolute inset-0 opacity-30 transition-opacity duration-700">
+      <div className="absolute inset-0 opacity-30 transition-opacity duration-700">
         <div className="absolute inset-0 bg-gradient-to-b from-violet-900/50 via-violet-900/50 to-black z-10" />
-        <Image1 className="w-full h-full object-cover scale-105 hover:scale-100 transition-transform duration-1000" />
-      </div> */}
+        {/* <Image1 className="w-full h-full object-cover scale-105 hover:scale-100 transition-transform duration-1000" /> */}
+      </div>
       {/* Content */}
       <div className="relative z-10 max-w-5xl mx-auto space-y-8 text-center">
         <h1 className="text-4xl md:text-7xl text-white font-bold mb-4 leading-tight animate-fade-in-up tracking-tight flex flex-col items-center gap-3 text-shadow-lg">
